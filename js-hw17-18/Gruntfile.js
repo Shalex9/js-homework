d93a514ec15d4037e0a7d910ae79b0c67c2bdd92
shalex9 <shalex9@gmail.com>
2016-11-22 12:42:41 +0200

    min.CSS

--- js-hw17-18/Gruntfile.js
+++ js-hw17-18/Gruntfile.js
@@ -10,6 +10,9 @@ module.exports = function(grunt) {
       },
     },
     concat: {
+      options: {
+        separator: ';',
+      },
       dist: {
         src: ['css/*.css'],
         dest: 'dist/css/production.css',
@@ -21,46 +24,47 @@ module.exports = function(grunt) {
           dest: 'dist/js/production.min.js'
       }
     },
-    uglify: {
-      build: {
-          src: 'dist/css/production.css',
-          dest: 'dist/css/production.min.css'
-      }
+    sass: {
+        dist: {
+            options: {
+                style: 'compressed'
+            },
+            files: {
+                'dist/css/global.css': 'css/*.css'
+            }
+        }
     },
     imagemin: {
       dynamic: {
           files: [{
               expand: true,
               cwd: 'images/',
-              src: ['**/*.{png,jpg,gif}'],
-              dest: 'dist/images/'
+              src: ['img/*.{png,jpg,gif}'],
+              dest: 'dist/img/'
           }]
       }
     },
-    // watch: {
-    //   scripts: {
-    //       files: ['jlider.js', 'js/*.js'],
-    //       tasks: ['concat', 'uglify', 'imagemin', 'sass'],
-    //       options: {
-    //           spawn: false,
-    //       },
-    //   }
-    // },
-    // sass: {
-    //   dist: {
-    //       options: {
-    //           style: 'compressed'
-    //       },
-    //       files: {
-    //           'dist/css/global.css': 'css/css.scss'
-    //       }
-    //   }
-    // }
+    watch: {
+      scripts: {
+          files: ['jlider.js', 'js/*.js'],
+          tasks: ['concat', 'uglify', 'imagemin', 'sass'],
+          options: {
+              spawn: false,
+          },
+      },
+      css: {
+          files: ['css/*.scss'],
+          tasks: ['sass'],
+          options: {
+              spawn: false,
+          }
+      }
+    }
   });
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.registerTask('default', ['concat', 'uglify', 'imagemin']);
+  grunt.registerTask('default', ['concat', 'uglify', 'imagemin', 'sass']);
 };
