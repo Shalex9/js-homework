108818377a1c4adab59037b0304e54988a172e8b
shalex9 <shalex9@gmail.com>
2016-11-23 23:37:52 +0200

    masonry

--- js-finalExam/dist/js/main.js
+++ js-finalExam/dist/js/main.js
@@ -1,1 +1,12 @@
+// MASONRY
+// (function() {
+//   $('.grid').isotope({
+//     layoutMode: 'packery',
+//     itemSelector: '.grid-item',
+//     percentPosition: true,
+//     packery: {
+//       gutter: '.gutter-sizer'
+//     }
+//   });
+// })
 "use strict";
--- js-finalExam/src/js/main.js
+++ js-finalExam/src/js/main.js
@@ -1,1 +1,11 @@
-
+// MASONRY
+// (function() {
+//   $('.grid').isotope({
+//     layoutMode: 'packery',
+//     itemSelector: '.grid-item',
+//     percentPosition: true,
+//     packery: {
+//       gutter: '.gutter-sizer'
+//     }
+//   });
+// })
