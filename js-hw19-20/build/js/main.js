3b4dab682413a2d86a69b05f076bef07bd933b3a
shalex9 <shalex9@gmail.com>
2016-11-24 15:58:58 +0200

    BEM
    переделал классы под нормальный БЭМ

--- js-hw19-20/build/js/main.js
+++ js-hw19-20/build/js/main.js
@@ -1,47 +1,47 @@
 $(document).ready(function(){
 
   // Slider
-  $(".firstArticle__slider__nav__outside__inside:first").css("display", "block");
-  $(".firstArticle__slider__nav__outside").click(function() {
-      $(this).find(".firstArticle__slider__nav__outside__inside").css("display", "block");
-      $(this).siblings(".firstArticle__slider__nav__outside").find(".firstArticle__slider__nav__outside__inside").css("display", "none")
+  $(".slider__nav__inside:first").css("display", "block");
+  $(".slider__nav__outside").click(function() {
+      $(this).find(".slider__nav__inside").css("display", "block");
+      $(this).siblings(".slider__nav__outside").find(".slider__nav__inside").css("display", "none")
   });
-  $(".firstArticle__slider__list__element__img1").css("opacity", "1");
-  $(".firstArticle__slider__nav__outside:first").click(function() {
-      $(".firstArticle__slider__list__element__img1").animate({opacity: "1"}, 1200);
-      $(".firstArticle__slider__list__element__img2, .firstArticle__slider__list__element__img3").animate({opacity: "0"}, 1200);
+  $(".slider__img1").css("opacity", "1");
+  $(".slider__nav__outside:first").click(function() {
+      $(".slider__img1").animate({opacity: "1"}, 1200);
+      $(".slider__img2, .slider__img3").animate({opacity: "0"}, 1200);
   });
-  $(".firstArticle__slider__nav__outside:nth-child(2)").click(function() {
-      $(".firstArticle__slider__list__element__img2").animate({opacity: "1"}, 1200);
-      $(".firstArticle__slider__list__element__img1, .firstArticle__slider__list__element__img3").animate({opacity: "0"}, 1200);
+  $(".slider__nav__outside:nth-child(2)").click(function() {
+      $(".slider__img2").animate({opacity: "1"}, 1200);
+      $(".slider__img1, .slider__img3").animate({opacity: "0"}, 1200);
   });
-  $(".firstArticle__slider__nav__outside:last-child").click(function() {
-      $(".firstArticle__slider__list__element__img3").animate({opacity: "1"}, 1200);
-      $(".firstArticle__slider__list__element__img1, .firstArticle__slider__list__element__img2").animate({opacity: "0"}, 1200);
+  $(".slider__nav__outside:last-child").click(function() {
+      $(".slider__img3").animate({opacity: "1"}, 1200);
+      $(".slider__img1, .slider__img2").animate({opacity: "0"}, 1200);
   });
   // END Slider
 
   // Голубой фон при наведении на картинки
-  $(".ourServices__content__item__items__img__picture").mouseover(function() {
-    $(this).find(".ourServices__content__item__items__img__picture__fon").css("display", "block")})
+  $(".services__box-img").mouseover(function() {
+    $(this).find(".services__box-img__fon").css("display", "block")})
     .mouseout(function() {
-        $(this).find(".ourServices__content__item__items__img__picture__fon").css("display", "none")});
+        $(this).find(".services__box-img__fon").css("display", "none")});
   // конец фона
 
   //   Accordion
-    $(".news__block__blockRight__accordion__title:first").addClass("active")
-        .find(".news__block__blockRight__accordion__title__plusMinus").addClass("activePlus").text("-");
-    $(".news__block__blockRight__accordion__p:not(:first)").hide();
+    $(".accordion__title-box:first").addClass("active")
+        .find(".accordion__plus-minus").addClass("active-plus").text("-");
+    $(".accordion__text:not(:first)").hide();
 
-    $(".news__block__blockRight__accordion__title").click(function(){
-        $(this).next(".news__block__blockRight__accordion__p").slideToggle("slow")
-            .siblings(".news__block__blockRight__accordion__p:visible").slideUp("slow");
-        var text = $(this).find(".news__block__blockRight__accordion__title__plusMinus").text();
+    $(".accordion__title-box").click(function(){
+        $(this).next(".accordion__text").slideToggle("slow")
+            .siblings(".accordion__text:visible").slideUp("slow");
+        var text = $(this).find(".accordion__plus-minus").text();
         $(this).toggleClass("active")
-            .find(".news__block__blockRight__accordion__title__plusMinus").toggleClass("activePlus").text(text == "+" ? "-" : "+")
-            .siblings(".news__block__blockRight__accordion__title").removeClass("active")
-            .find(".news__block__blockRight__accordion__title__plusMinus").text(text == "+" ? "-" : "+");
-        // var sibl = $(this).siblings(".news__block__blockRight__accordion__title").find(".news__block__blockRight__accordion__title__plusMinus");
+            .find(".accordion__plus-minus").toggleClass("active-plus").text(text == "+" ? "-" : "+")
+            .siblings("accordion__title-box").removeClass("active")
+            .find(".accordion__plus-minus").text(text == "+" ? "-" : "+");
+        // var sibl = $(this).siblings(".accordion__title-box").find(".accordion__plus-minus");
         // (sibl.text()==="+" ? "-" : "+");
      });
      //  END Accordion
